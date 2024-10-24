f983782046075cadab920e5146a045b057aadbc7
Kto-_-to <fomiknihdima@gmail.com>
2024-10-24 20:52:52 +0300

    add borrr

---
+++ bor_for_kay.h
@@ -0,0 +1,18 @@
+//
+// Created by Дмитрий Фоминых on 24.10.2024.
+//
+
+#ifndef MEGO_TRUE_LANGUEG_BOR_FOR_KAY_H
+#define MEGO_TRUE_LANGUEG_BOR_FOR_KAY_H
+
+#endif //MEGO_TRUE_LANGUEG_BOR_FOR_KAY_H
+
+class bor() {
+public:
+    struct node {
+        int a;
+    };
+    node* _start;
+private:
+    bool _suka;
+}
