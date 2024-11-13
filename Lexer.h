a9b280d8b29af4baf19f2bae780f43a02cfcdb34
KorolSkeletov2011 <ivangusevklin@gmail.com>
2024-11-13 13:56:58 +0300

    OPOPOP

---
+++ Lexer.h
@@ -0,0 +1,54 @@
+#ifndef MEGO_TRUE_LANGUEG_LEXER_H
+#define MEGO_TRUE_LANGUEG_LEXER_H
+
+void Program() {}
+
+void Main() {}
+
+void Function_declarations() {}
+
+void Description_of_functions() {}
+
+void Type() {}
+
+void Array() {}
+
+void Declaring_variables() {}
+
+void Declaring_arguments() {}
+
+void Declaring_variable() {}
+
+void Symbol() {}
+
+void Number() {}
+
+void Double() {}
+
+void Name() {}
+
+void String() {}
+
+void Literal() {}
+
+void Code_block() {}
+
+void Keyword() {}
+
+void Exp_main() {}
+
+void While() {}
+
+void For() {}
+
+void Between() {}
+
+void If() {}
+
+void Else() {}
+
+void Switch() {}
+
+void Case() {}
+
+#endif //MEGO_TRUE_LANGUEG_LEXER_H
